c1eb447ef579467eebca748033f2573fe8d346e9
Brus Palaj <brusp@bruss-mbp.home>
2020-05-27 17:05:52 -0400

    heroku mLab mongodb connection added to server.js

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"; 
+
+mongoose.connect(MONGODB_URI); 
+
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));
